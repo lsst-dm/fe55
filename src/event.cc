233724066c1500ee0245c77c1fcf3e285c35675d
Robert Lupton the Good <rhl@astro.princeton.edu>
2012-12-20 15:30:28 -0500

    Set the data[] array from the Image

--- src/event.cc
+++ src/event.cc
@@ -12,19 +12,29 @@ Event::Event(afw::image::Image<float> const& im,        // image containing even
              int chipnum_                        // chip ID for image
             )
 {
-    framenum = framenum_;
-    chipnum = chipnum_;
-    x = cen.getX();
-    y = cen.getY();
-    mode = 0;
-
     if (!im.getBBox(afw::image::PARENT).contains(cen)) {
         throw LSST_EXCEPT(lsst::pex::exceptions::OutOfRangeException,
                           str(boost::format("%d is not contained in %d")
                               % cen % im.getBBox(afw::image::PARENT)));
     }
 
-    //float data[9];
+    framenum = framenum_;
+    chipnum = chipnum_;
+    x = cen.getX();
+    y = cen.getY();
+    mode = 0;
+
+    afw::image::Image<float>::xy_locator imData = im.xy_at(cen.getX(), cen.getY());
+    int i = 0;
+    data[i++] = imData(-1, -1);
+    data[i++] = imData( 0, -1);
+    data[i++] = imData( 1, -1);
+    data[i++] = imData(-1,  0);
+    data[i++] = imData( 0,  0);
+    data[i++] = imData( 1,  0);
+    data[i++] = imData(-1,  1);
+    data[i++] = imData( 0,  1);
+    data[i++] = imData( 1,  1);
 }
         
 }
